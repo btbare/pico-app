a449a138f0e7ee43f9e076cdafd9458d323fd198
btbare <btbare2002@gmail.com>
2026-02-03 22:11:05 -0500

    Practice using Pico.css in Next.js app

---
+++ src/app/about/page.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+export default function About(){
+    
+    return(
+
+        <>
+    
+        <h1 style={{ 
+            fontFamily: "cursive",
+            margin: "10px",
+            padding: "10px",
+        }}>ABOUT PAGE</h1>
+
+
+        <div style={{ textAlign:"center" }}>
+            
+            <p>Below this line is a button<br></br>don't click it</p>
+
+            <hr></hr>
+
+            <button style={{
+                color: "black",
+                fontFamily: "math",
+                backgroundColor:"maroon",
+            }}
+            onClick={() => alert("                                                                                                                                                                         wtf dude     (° ͡ ʖ_° ͡) ")}
+            >NO TOUCHING!</button>
+
+            <hr></hr>
+
+            <h6 style={{
+                color:"grey",
+                fontFamily:"emoji"
+            }}
+            >please, do not click the button</h6>
+        </div>
+        </>
+    )
+}
