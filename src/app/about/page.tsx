2cf558776660b805990aae4da2a8dfc1b6c8e45f
btbare <btbare2002@gmail.com>
2026-02-03 22:56:56 -0500

    reformat/lint

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,40 +1,38 @@
 "use client"
 
-export default function About(){
-    
-    return(
+export default function About() {
 
+    return (
         <>
-    
-        <h1 style={{ 
-            fontFamily: "cursive",
-            margin: "10px",
-            padding: "10px",
-        }}>ABOUT PAGE</h1>
+            <h1 style={{
+                fontFamily: "cursive",
+                margin: "10px",
+                padding: "10px",
+            }}>ABOUT PAGE</h1>
 
+            <div style={{ textAlign: "center" }}>
 
-        <div style={{ textAlign:"center" }}>
-            
-            <p>Below this line is a button<br></br>don't click it</p>
+                <p>Below this line is a button<br></br>don't click it</p>
 
-            <hr></hr>
+                <hr></hr>
 
-            <button style={{
-                color: "black",
-                fontFamily: "math",
-                backgroundColor:"maroon",
-            }}
-            onClick={() => alert("                                                                                                                                                                         wtf dude     (° ͡ ʖ_° ͡) ")}
-            >NO TOUCHING!</button>
+                <button style={{
+                    color: "black",
+                    fontFamily: "math",
+                    backgroundColor: "maroon",
+                }}
+                    onClick={() => alert("                                                                                                                                                                                              wtf dude     (° ͡ ʖ_° ͡) ")}
+                >NO TOUCHING!</button>
 
-            <hr></hr>
+                <hr></hr>
 
-            <h6 style={{
-                color:"grey",
-                fontFamily:"emoji"
-            }}
-            >please, do not click the button</h6>
-        </div>
+                <h6 style={{
+                    color: "grey",
+                    fontFamily: "emoji"
+                }}
+                >please, do not click the button</h6>
+
+            </div>
         </>
     )
 }
